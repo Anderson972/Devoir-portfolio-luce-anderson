1a8426fc2fb87c93667f7406e1b32cf700722ddc
Anderson LUCE <anderson.luce@gmail.com>
2026-03-04 13:40:44 +0100

    ajout code pour page portfolio

--- src/pages/service.jsx
+++ src/pages/service.jsx
@@ -9,29 +9,29 @@ const Services = () => {
                 <p>Voici les prestations sur lesquelles je peux intervenir</p>
                 <hr className="border border-primary border-3 opacity-100" style={{width: 30 + '%'}} />
                 <div className="cards">
-                    <div class="card text-center mb-3">
-                        <div class="card-body">
-                            <i class="bi bi-brush text-primary" style={{fontSize: 3 +'rem'}}></i>
-                            <h2 class="card-title">UX Design</h2>
-                            <p class="card-text">
+                    <div className="card text-center mb-3">
+                        <div className="card-body">
+                            <i className="bi bi-brush text-primary" style={{fontSize: 3 +'rem'}}></i>
+                            <h2 className="card-title">UX Design</h2>
+                            <p className="card-text">
                                 L'UX Design est une discipline qui consiste à concevoir des produits (sites web, applications mobiles, logiciels, objets connectés, etc.) en plaçant
                                 l'utilisateur au centre des préoccupations. L'objectif est de rendre l'experience utilisateur la plus fuide et agréable possible.</p>
                         </div>
                     </div>
-                    <div class="card text-center mb-3">
-                        <div class="card-body">
-                            <i class="bi bi-code-slash text-primary" style={{fontSize: 3 +'rem'}}></i>
-                            <h2 class="card-title">Développement web</h2>
-                            <p class="card-text">
+                    <div className="card text-center mb-3">
+                        <div className="card-body">
+                            <i className="bi bi-code-slash text-primary" style={{fontSize: 3 +'rem'}}></i>
+                            <h2 className="card-title">Développement web</h2>
+                            <p className="card-text">
                                 Le développement de sites web consiste à créer des sites internet en utilisant des langages de programmation (HTML, CSS, JavaScript, PHP, etc.)
                                 et des frameworks (Bootstrap, React, Angular, etc.).</p>
                         </div>
                     </div>
-                    <div class="card text-center mb-3">
-                        <div class="card-body">
-                            <i class="bi bi-search text-primary" style={{fontSize: 3 +'rem'}}></i>
-                            <h2 class="card-title">Référencement</h2>
-                            <p class="card-text">
+                    <div className="card text-center mb-3">
+                        <div className="card-body">
+                            <i className="bi bi-search text-primary" style={{fontSize: 3 +'rem'}}></i>
+                            <h2 className="card-title">Référencement</h2>
+                            <p className="card-text">
                                 Le référencement naturel (SEO) est une technique qui consiste à optimiser un site web pour le faire remonter dans les résulas des moteur de recherche (Google, Bing, Yahoo, etc.). 
                                 L'objectif est d'attirer un maximun de visiteurs qualifiés sur le site.</p>
                         </div>
